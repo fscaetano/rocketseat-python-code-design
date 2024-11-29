0bfd03f8854150309db24a5c9d03d3f072d976d4
Fabricio da Silva Caetano <fscaetano@gmail.com>
2024-11-29 19:56:12 -0300

    Implemented calculator factories

--- src/main/routes/calculators.py
+++ src/main/routes/calculators.py
@@ -1,8 +1,6 @@
 from flask import Blueprint, jsonify, request
-from src.calculators.calculator_1 import Calculator1
-from src.calculators.calculator_2 import Calculator2
-from src.drivers.numpy_handler import NumpyHandler
-
+from src.main.factories.calculator1_factory import calculator1_factory
+from src.main.factories.calculator2_factory import calculator2_factory
 
 calc_route_bp = Blueprint("calc_routes", __name__)
 
@@ -14,7 +12,6 @@ def calculator_1():
 
 @calc_route_bp.route("/calculator/2", methods=["POST"])
 def calculator_2():
-    numpy_handler = NumpyHandler()
-    calc = Calculator2(numpy_handler)
+    calc = calculator2_factory()
     response = calc.calculate(request)
     return jsonify(response), 200
